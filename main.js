0c31b5ac67d1222738dfb22f176bff8e442e8669
Teo3tc <theo.etchanchu@gmail.com>
2021-07-16 17:58:41 +0200

    paralax

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-import './assets/scss/main.scss'
-import 'lazysizes';
+import "./assets/scss/main.scss";
+import "lazysizes";
 
-const btnMenu = document.getElementById('menu-btn')
-const btnMenuOpen = document.getElementById('menu-btn__open')
-const btnMenuClose = document.getElementById('menu-btn__close')
-const menu = document.getElementById('menu')
+const btnMenu = document.getElementById("menu-btn");
+const btnMenuOpen = document.getElementById("menu-btn__open");
+const btnMenuClose = document.getElementById("menu-btn__close");
+const menu = document.getElementById("menu");
 
-btnMenu.addEventListener('click',()=>{
-  btnMenuClose.classList.toggle('menuActive')
-  btnMenuOpen.classList.toggle('menuActive')
-  menu.classList.toggle('menuActive')
-})
+btnMenu.addEventListener("click", () => {
+  btnMenuClose.classList.toggle("menuActive");
+  btnMenuOpen.classList.toggle("menuActive");
+  menu.classList.toggle("menuActive");
+});
 
-
+window.addEventListener("scroll", () => {
+  const target = [...document.querySelectorAll(".js-parax")];
+  console.log(target);
+  console.log(123);
+  target.forEach((el) => {
+    const pos = window.pageYOffset * el.dataset.speed;
+    console.log(pos);
+    el.style.transform = `translate3d(0px,${pos}px,0px)`;
+  });
+});
